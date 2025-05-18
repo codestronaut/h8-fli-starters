c325dd12a8ab82061a93ae5b6326b8aef4441abc
adityarohman <adityarhmaan@gmail.com>
2025-05-18 21:27:22 +0700

    add: session-4 update todo

--- h8_fli_geo_maps_starter/ios/Runner/AppDelegate.swift
+++ h8_fli_geo_maps_starter/ios/Runner/AppDelegate.swift
@@ -7,6 +7,8 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // TODO: Add your API Key from Google Cloud Platform
+    // GMSServices.provideAPIKey("YOUR KEY HERE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- h8_fli_geo_maps_starter/lib/view/geo_view.dart
+++ h8_fli_geo_maps_starter/lib/view/geo_view.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:h8_fli_geo_maps_starter/manager/geo_bloc.dart';
+import 'package:h8_fli_geo_maps_starter/model/geo.dart';
 
-class GeoView extends StatelessWidget {
+class GeoView extends StatefulWidget {
   const GeoView({super.key});
+
+  @override
+  State<GeoView> createState() => _GeoViewState();
+}
+
+class _GeoViewState extends State<GeoView> {
+  // TODO: 1. Declare the GoogleMapController.
+  // TODO: 2. Declate the CameraPosition for the initial location.
+
+  @override
+  void initState() {
+    // TODO: 3. Initialize the GoogleMapController.
+    // TODO: 4. Initialize the CameraPosition for the initial location.
+    /// Provide the following paremeters:
+    /// - target: initial location.
+    /// - zoom: initial zoom level.
+    super.initState();
+  }
+
+  // TODO: 5. Complete the _updateCameraPosition() implementation.
+  void _updateCameraPosition(Geo geo) {}
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +38,7 @@ class GeoView extends StatelessWidget {
               child: switch (state) {
                 GeoInitial() => SizedBox(),
                 GeoLoading() => CircularProgressIndicator(),
+                // TODO: 6. Update the widget to display GoogleMap widget.
                 GeoLoaded() => Column(
                   spacing: 8.0,
                   mainAxisSize: MainAxisSize.min,
