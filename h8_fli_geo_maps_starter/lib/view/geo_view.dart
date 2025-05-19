d5622aeff922afc330e283122471dbfceb26cd3e
adityarohman <adityarhmaan@gmail.com>
2025-05-19 21:08:34 +0700

    add: session-4 update todo

--- h8_fli_geo_maps_starter/lib/view/geo_view.dart
+++ h8_fli_geo_maps_starter/lib/view/geo_view.dart
@@ -11,12 +11,12 @@ class GeoView extends StatefulWidget {
 }
 
 class _GeoViewState extends State<GeoView> {
-  // TODO: 1. Declare the GoogleMapController.
+  // TODO: 1. Declare the Completer<GoogleMapController>.
   // TODO: 2. Declate the CameraPosition for the initial location.
 
   @override
   void initState() {
-    // TODO: 3. Initialize the GoogleMapController.
+    // TODO: 3. Initialize the Completer<GoogleMapController>.
     // TODO: 4. Initialize the CameraPosition for the initial location.
     /// Provide the following paremeters:
     /// - target: initial location.
